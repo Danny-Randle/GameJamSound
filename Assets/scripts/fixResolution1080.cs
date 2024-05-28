61f61a5311db286cd1273032a41c293047ba4330
Danny-Randle <DANNY.RANDLE23@bathspa.ac.uk>
2024-05-28 10:49:58 +0100

    fix for collision

---
+++ Assets/scripts/fixResolution1080.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fixResolution1080 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set the resolution:
+        Screen.SetResolution(1920, 1080, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
